47cddfaa1b0b01af34d9a87ebbab374463711afd
Kanaishk Garg <kanaishkgarg@gmail.com>
2021-11-06 23:57:14 +0530

    Adding custom font to banner

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import './Navigation.css';
 
 class Navigation extends React.Component {
 	render() {
     	return (
         	<Navbar collapseOnSelect fixed='top' expand='lg' bg='dark' variant="dark">
         		<Container>
-        			<Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+        			<Navbar.Brand href="/"><span className='font_ths'>React-Bootstrap</span></Navbar.Brand>
         			<Navbar.Toggle aria-controls='basic-navbar-nav' />
         			<Navbar.Collapse id='basic-navbar-nav'>
         				<Nav className='me-auto'>
